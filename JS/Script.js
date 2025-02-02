9aaa3908827c753bd6f59dda5ff24d7d7f9daedd
Gustavo Alves <161645395+GustavoAlvHnr@users.noreply.github.com>
2025-02-02 14:23:37 -0300

    Update Script.js

--- JS/Script.js
+++ JS/Script.js
@@ -1,7 +1,10 @@
+//Abre a fecha a barra lateral
 function toggleSidebar(){
     var sidebar = document.getElementById("sidebar");
     sidebar.style.right = (sidebar.style.right === "0px") ? "-270px" : "0px"
 }
+
+//Fecha a barra lateral quando clicar fora da barra
 document.addEventListener("click", function(event){
     var sidebar = document.getElementById("sidebar");
     var menuBtn = document.querySelector(".menu-btn");
